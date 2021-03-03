9da62534ea0e87e3eb320a138f260ab2aced8269
guiisgooey <49354894+guiisgooey@users.noreply.github.com>
2021-03-03 02:57:27 -0500

    created div class dependency on what weather
    class is now dependent on the weather type

--- spd-project/src/components/CurrentWeatherMain.js
+++ spd-project/src/components/CurrentWeatherMain.js
@@ -9,6 +9,7 @@ const useGeolocate = () => {
   const [loading, setLoading] = useState(true);
   const [date, setDate] = useState("Date");
   const [time, setTime] = useState("0:00:00");
+  const [weather, setWeather] = useState("01d");
 
   useEffect(() => {
     if ("geolocation" in navigator) {
@@ -28,6 +29,8 @@ const useGeolocate = () => {
           `${dateObj.getHours()}:${dateObj.getMinutes()}:${dateObj.getSeconds()}`
         );
 
+        setWeather(weatherClass(data.weather[0].icon));
+
         setLoading(false);
       });
     } else {
@@ -35,11 +38,23 @@ const useGeolocate = () => {
     }
   }, []);
 
-  return { error, data, loading, date, time };
+  return { error, data, loading, date, time, weather };
 };
 
+function weatherClass(weather) {
+  let map = {};
+  map[("01d", "01n")] = "weather-main-current-clear-sky";
+  map[("02d", "02n", "03d", "03n", "04d", "04n")] =
+    "weather-main-current-clouds";
+  map[("09d", "09n", "10d", "10n")] = "weather-main-current-rain";
+  map[("11d", "11n")] = "weather-main-current-thunderstorm";
+  map[("13d", "13n")] = "weather-main-current-snow";
+  map[("50d", "50n")] = "weather-main-current-mist";
+  return map[weather];
+}
+
 export default () => {
-  const { error, data, loading, date, time } = useGeolocate();
+  const { error, data, loading, date, time, weather } = useGeolocate();
   if (!error) {
     return (
       <>
@@ -50,40 +65,46 @@ export default () => {
             <div class="weather-main-date">{date}</div>
           )}
         </div>
-        <div class="weather-main-current">
-          {loading ? (
-            <div class="weather-main-locat">...loading</div>
-          ) : (
-            <div class="weather-main-locat">
-              {data.name}, {data.sys.country}
-            </div>
-          )}
-          {loading ? (
-            <div class="weather-main-dt">...loading</div>
-          ) : (
-            <div class="weather-main-dt">{time}</div>
-          )}
-          {loading ? (
-            <div class="weather-main-temp">...loading</div>
-          ) : (
-            <div class="weather-main-temp">
-              {Math.floor(data.main.temp - k)}° C
-            </div>
-          )}
-          {loading ? (
-            <div class="weather-main-temp-hilo">...loading</div>
-          ) : (
-            <div class="weather-main-temp-hilo">
-              {Math.floor(data.main.temp_max - k)}° C /
-              {Math.floor(data.main.temp_min - k)}° C
-            </div>
-          )}
-          {loading ? (
-            <div class="weather-main-desc">...loading</div>
-          ) : (
-            <div class="weather-main-desc">{data.weather[0].description}</div>
-          )}
-        </div>
+        {loading ? (
+          <div class="weather-main-current-loading">
+            Loading Weather Data Please Wait
+          </div>
+        ) : (
+          <div class={weather}>
+            {loading ? (
+              <div class="weather-main-locat">...loading</div>
+            ) : (
+              <div class="weather-main-locat">
+                {data.name}, {data.sys.country}
+              </div>
+            )}
+            {loading ? (
+              <div class="weather-main-dt">...loading</div>
+            ) : (
+              <div class="weather-main-dt">{time}</div>
+            )}
+            {loading ? (
+              <div class="weather-main-temp">...loading</div>
+            ) : (
+              <div class="weather-main-temp">
+                {Math.floor(data.main.temp - k)}° C
+              </div>
+            )}
+            {loading ? (
+              <div class="weather-main-temp-hilo">...loading</div>
+            ) : (
+              <div class="weather-main-temp-hilo">
+                {Math.floor(data.main.temp_max - k)}° C /
+                {Math.floor(data.main.temp_min - k)}° C
+              </div>
+            )}
+            {loading ? (
+              <div class="weather-main-desc">...loading</div>
+            ) : (
+              <div class="weather-main-desc">{data.weather[0].description}</div>
+            )}
+          </div>
+        )}
       </>
     );
   } else {
